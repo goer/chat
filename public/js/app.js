a4c9c127cabd2f719713c468beaf9a214cc10f73
Guruh Purnama <goer@Mac-mini.local>
2015-04-01 22:14:56 +0700

    openshift

---
+++ public/js/app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var app = angular.module('chat',[]);
+
+app.factory('socket', function($rootScope) { 
+	var socket = io.connect('localhost:8000');
+	return {
+		on: function(eventName, callback) {
+			socket.on(eventName, function() {
+				var args = arguments;
+				$rootScope.$apply(function() {
+					callback.apply(socket, args);
+				});
+			});
+		},
+		emit: function(eventName, data, callback) {
+			socket.emit(eventName, data, function() {
+				var args = arguments;
+				$rootScope.$apply(function() {
+					if(callback) {
+						callback.apply(socket, args);
+					}
+				});
+			});
+		}
+	};
+});
+
+app.controller('MainCtrl',function($scope,socket){
+
+	$scope.messages = [];
+
+	socket.on('message',function(data){
+		console.log(data);
+		$scope.messages.push(data);
+	});
+
+	$scope.sendMessage = function(){
+		socket.emit('send', {message : $scope.message });	
+		console.log('message:'+$scope.message);
+	};
+
+
+});
+
